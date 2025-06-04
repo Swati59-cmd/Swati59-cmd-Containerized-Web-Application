3f8509497c7305169a28a44ffa07e5a4e35b225e
Swati59-cmd <swati.h@cloudshastrainfotech.com>
2025-06-04 21:24:06 +0530

    Update variables.tf

--- CICD/variables.tf
+++ CICD/variables.tf
@@ -1,21 +1,26 @@
 variable "env" {
   type = string
+  default = "dev"
 }
 variable "bucket_name" {
   type = string
-
+  default = "cicddevarti
 }
 
 variable "github_branch" {
   type = string
-
+  default = "dev"
 }
 
 variable "ecs_cluster" {
   type = string
+  default = "dev-ecs-cluster"
 
 }
-variable "ecs_service" {}
+variable "ecs_service" {
+   type= string
+  default = "dev-service"
+}
 
 variable "aws_account_id" {
   type    = number
