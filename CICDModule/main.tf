37bc505da325ae5ece4a2d8a9b3dd1b067d931ab
Swati59-cmd <swati.h@cloudshastrainfotech.com>
2025-06-04 23:38:14 +0530

    Create main.tf

---
+++ CICDModule/main.tf
@@ -0,0 +1,9 @@
+module "cicd" {
+  source         = "../CICD"
+  env            = "main"
+  bucket_name    = var.bucket_name
+  github_branch  = var.github_branch
+  ecs_cluster    = var.ecs_cluster
+  ecs_service    = var.ecs_service
+  aws_account_id = var.aws_account_id
+}
