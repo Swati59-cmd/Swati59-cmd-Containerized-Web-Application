4822fe238e1ef805a4f155213cb2c505433136f2
swati <swatihivanse78@gmail.com>
2025-06-04 22:26:18 +0530

    addmain

---
+++ terraformcode/variables.tf
@@ -0,0 +1,64 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "main"
+}
+
+variable "ami_id" {
+  description = "AMI ID for ECS EC2 instances"
+  type        = string
+  default     = "ami-0953476d60561c955"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for ECS cluster"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+  default     = "lambda"
+}
+
+variable "image" {
+  description = "Docker image to be used in ECS task"
+  type        = string
+  default     = "648908580279.dkr.ecr.us-east-1.amazonaws.com/devpython"
+}
+variable "subnet_ids" {
+  description = "List of subnet IDs for ALB"
+  type        = list(string)
+  default     = ["subnet-0d5742da73d46d482", "subnet-0b418610996a5bbc3"]
+}
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-00ac399f2759babf5"
+}
+variable "alb_target_group_arn" {
+  description = "ARN of the ALB Target Group"
+  type        = string
+  default     = "aws_lb_target_group.tg.arn"
+}
+
+variable "alb_listener_arn" {
+  description = "ARN of the ALB Listener"
+  type        = string
+  default     = " aws_lb_listener.listener.arn"
+}
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
