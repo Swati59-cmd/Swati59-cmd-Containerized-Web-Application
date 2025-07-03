28b93d974ae0777b556eaed1cf9d183949967fcc
Swati59-cmd <swati.h@cloudshastrainfotech.com>
2025-07-03 12:38:41 +0530

    Delete CICDmodule directory

--- CICDmodule/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "statefilevpc"
-    key    = "Pipline/terraform.tfstate"
-    region = "us-east-1"
-
-  }
-}
--- CICDmodule/main.tf
+++
@@ -1,9 +0,0 @@
-module "cicd" {
-  source         = "../CICD"
-  env            = "dev"
-  bucket_name    = var.bucket_name
-  github_branch  = var.github_branch
-  ecs_cluster    = var.ecs_cluster
-  ecs_service    = var.ecs_service
-  aws_account_id = var.aws_account_id
-}
