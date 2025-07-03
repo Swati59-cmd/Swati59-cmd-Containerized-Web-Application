caa7fd3ec95b6080ca4a402ef7e4a677a568bd08
swati <swatihivanse78@gmail.com>
2025-07-03 16:19:03 +0530

    add path in ecs

--- Environment/ECS/main.tf
+++ Environment/ECS/main.tf
@@ -140,7 +140,7 @@ data "aws_ssm_parameter" "ecs_ami" {
 }
 
 module "vpc" {
-  source               = "../VPC-dev"
+  source               = "../VPC"
   vpc_cidr             = "10.0.0.0/16"
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24"]
