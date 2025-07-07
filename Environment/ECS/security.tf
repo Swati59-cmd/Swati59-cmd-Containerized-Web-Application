f0b56d61100467921afa5ed6a1313e5a5953f18c
swati <swatihivanse78@gmail.com>
2025-07-07 15:10:10 +0530

    add security group in ecs

---
+++ Environment/ECS/security.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "ecs_sg" {
+  name   = "${var.environment}-ecs-sg"
+  vpc_id = module.vpc_stage.vpc_id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-ecs-sg"
+  }
+}
+resource "aws_security_group" "alb_sg" {
+  name   = "${var.environment}-alb-sg"
+  vpc_id = module.vpc_stage.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
