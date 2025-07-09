98f060e50657964c962c2e7eecda1305a201776c
swati <swatihivanse78@gmail.com>
2025-07-09 15:54:59 +0530

    add

--- Environment/ECS/main.tf
+++ Environment/ECS/main.tf
@@ -33,7 +33,7 @@ resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
 }
 
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
-  name = "${var.environment}-ecs-instance-profiledemo"
+  name = "${var.environment}-ecs-instance"
   role = aws_iam_role.ecs_instance_role.name
 }
 
