d76104c41043678af85af5b8a67603b38ece50e6
swati <swatihivanse78@gmail.com>
2025-07-10 12:23:06 +0530

    add changes

--- modules/ECS/Backend.tf
+++ modules/ECS/Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "swatistatefile"
+    bucket = "swatitfstate"
     key    = "ECSStage/terraform.tfstate"
     region = "eu-north-1"
 
--- modules/VPC/Backend.tf
+++ modules/VPC/Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "swatistatefile"
+    bucket = "swatitfstate"
     key    = "vpc-stage/terraform.tfstate"
     region = "eu-north-1"
 
