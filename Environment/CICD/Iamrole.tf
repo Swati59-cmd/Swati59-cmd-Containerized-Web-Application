11a98e380cec52edf846f3145e7539ca3fe3af87
swati <swatihivanse78@gmail.com>
2025-07-16 17:54:35 +0530

    adding

--- Environment/CICD/Iamrole.tf
+++ Environment/CICD/Iamrole.tf
@@ -1,15 +1,22 @@
+
+
+# -----------------------------
+# CodePipeline IAM Role
+# -----------------------------
 resource "aws_iam_role" "codepipeline_role" {
   name = "${var.env}-codepipeline-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "codepipeline.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "codepipeline.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
       }
-    }]
+    ]
   })
 }
 
@@ -18,49 +25,42 @@ resource "aws_iam_role_policy_attachment" "codepipeline_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_ReadOnlyAccess"
 }
 
-resource "aws_iam_policy" "codepipeline_artifact_access" {
-  name = "${var.env}-CodePipelineArtifactS3Access"
+# Full S3 Admin Access Policy (Custom)
+resource "aws_iam_policy" "s3_admin_access" {
+  name = "${var.env}-S3FullAccess"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Sid    = "S3ArtifactReadWrite",
+        Sid:    "S3AdminAccess",
         Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:PutObject",
-          "s3:PutObjectAcl"
-        ],
+        Action = "s3:*",
         Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.codepipeline_artifacts.bucket}/dev-Pipeline/*"
+          "arn:aws:s3:::*",
+          "arn:aws:s3:::*/*"
         ]
-      },
-      {
-        Sid      = "S3ArtifactList",
-        Effect   = "Allow",
-        Action   = ["s3:ListBucket"],
-        Resource = "arn:aws:s3:::${aws_s3_bucket.codepipeline_artifacts.bucket}"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codepipeline_s3_attach" {
+resource "aws_iam_role_policy_attachment" "attach_codepipeline_s3_admin" {
   role       = aws_iam_role.codepipeline_role.name
-  policy_arn = aws_iam_policy.codepipeline_artifact_access.arn
+  policy_arn = aws_iam_policy.s3_admin_access.arn
 }
 
-# Allow CodeStar Connection
+# Allow CodePipeline to use CodeStar connection
 resource "aws_iam_policy" "codepipeline_codestar_connection" {
   name = "${var.env}-AllowCodeStarConnectionUse"
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Effect   = "Allow",
-      Action   = "codestar-connections:UseConnection",
-      Resource = "arn:aws:codeconnections:us-east-1:851725602228:connection/ab62ca85-1804-4eef-a633-058b9e8ff4cf"
-    }]
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "codestar-connections:UseConnection",
+        Resource = "arn:aws:codeconnections:us-east-1:${var.aws_account_id}:connection/ab62ca85-1804-4eef-a633-058b9e8ff4cf"
+      }
+    ]
   })
 }
 
@@ -69,16 +69,17 @@ resource "aws_iam_role_policy_attachment" "codepipeline_codestar_connection_atta
   policy_arn = aws_iam_policy.codepipeline_codestar_connection.arn
 }
 
-# CodePipeline can start CodeBuild
 resource "aws_iam_policy" "codepipeline_codebuild_access" {
   name = "${var.env}-CodePipelineStartBuildAccess"
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Effect   = "Allow",
-      Action   = ["codebuild:StartBuild", "codebuild:BatchGetBuilds"],
-      Resource = "*"
-    }]
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["codebuild:StartBuild", "codebuild:BatchGetBuilds"],
+        Resource = "*"
+      }
+    ]
   })
 }
 
@@ -87,7 +88,7 @@ resource "aws_iam_role_policy_attachment" "codepipeline_codebuild_access_attach"
   policy_arn = aws_iam_policy.codepipeline_codebuild_access.arn
 }
 
-# ECS Deploy Access
+# ECS Access from CodePipeline
 resource "aws_iam_policy" "codepipeline_ecs_access" {
   name = "${var.env}-CodePipelineECSAccess"
   policy = jsonencode({
@@ -106,8 +107,8 @@ resource "aws_iam_policy" "codepipeline_ecs_access" {
         Resource = "*"
       },
       {
-        Effect   = "Allow",
-        Action   = ["iam:PassRole"],
+        Effect = "Allow",
+        Action = ["iam:PassRole"],
         Resource = "*",
         Condition = {
           StringLike = {
@@ -124,22 +125,23 @@ resource "aws_iam_role_policy_attachment" "codepipeline_ecs_attach" {
   policy_arn = aws_iam_policy.codepipeline_ecs_access.arn
 }
 
-# ---------------------
+# -----------------------------
 # CodeBuild IAM Role
-# ---------------------
-
+# -----------------------------
 resource "aws_iam_role" "codebuild_role" {
   name = "${var.env}-codebuild-service-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "codebuild.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
       }
-    }]
+    ]
   })
 }
 
@@ -148,34 +150,9 @@ resource "aws_iam_role_policy_attachment" "codebuild_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
 
-resource "aws_iam_policy" "codebuild_s3_access" {
-  name = "${var.env}-CodeBuildS3Access"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "S3ArtifactAccess",
-        Effect = "Allow",
-        Action = [
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:GetObjectVersion"
-        ],
-        Resource = "arn:aws:s3:::${aws_s3_bucket.codepipeline_artifacts.bucket}/dev-Pipeline/*"
-      },
-      {
-        Sid      = "S3ArtifactList",
-        Effect   = "Allow",
-        Action   = ["s3:ListBucket"],
-        Resource = "arn:aws:s3:::${aws_s3_bucket.codepipeline_artifacts.bucket}"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_codebuild_s3_policy" {
+resource "aws_iam_role_policy_attachment" "attach_codebuild_s3_admin" {
   role       = aws_iam_role.codebuild_role.name
-  policy_arn = aws_iam_policy.codebuild_s3_access.arn
+  policy_arn = aws_iam_policy.s3_admin_access.arn
 }
 
 resource "aws_iam_policy" "codebuild_cloudwatch_logs" {
