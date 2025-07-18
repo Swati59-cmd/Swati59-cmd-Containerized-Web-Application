9916f98584576911c50b5caf10ec306f3a30ee80
swati <swatihivanse78@gmail.com>
2025-07-18 16:19:23 +0530

    addingone

--- Environment/ECS/ALB.tf
+++ Environment/ECS/ALB.tf
@@ -12,9 +12,10 @@ resource "aws_lb" "alb" {
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "${var.environment}-tg"
-  port     = 80
-  protocol = "HTTP"
+  name        = "${var.environment}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
 
   vpc_id = module.vpc.vpc_id
   tags = {
