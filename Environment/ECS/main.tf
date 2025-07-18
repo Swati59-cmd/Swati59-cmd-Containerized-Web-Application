a1cbb5584de58b17248234b6afb41be384ecd206
swati <swatihivanse78@gmail.com>
2025-07-18 16:16:56 +0530

    addingone

--- Environment/ECS/main.tf
+++ Environment/ECS/main.tf
@@ -143,8 +143,8 @@ resource "aws_ecs_task_definition" "task" {
       essential = true
       portMappings = [
         {
-          containerPort = 80,
-          hostPort      = 80
+          containerPort = 5000,
+          hostPort      = 5000
 
         }
       ]
