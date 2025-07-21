79fe0de6155305f0f115f13f20b3be4fde36f3e2
swati <swatihivanse78@gmail.com>
2025-07-21 17:18:17 +0530

    add files

--- modules/CICD/Backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "swatitfstate"
-    key    = "Pipline/terraform.tfstate"
-    region = "us-east-1"
-
-  }
-}
--- modules/CICD/main.tf
+++
@@ -1,8 +0,0 @@
-module "cicd" {
-  source         = "../../Environment/CICD"
-  env            = "dev"
-  github_branch  = var.github_branch
-  ecs_cluster    = var.ecs_cluster
-  ecs_service    = var.ecs_service
-  aws_account_id = var.aws_account_id
-}
