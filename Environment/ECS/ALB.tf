b21d8e191dff8ee9963f1eeffe6d1e354aab11b4
swati <swatihivanse78@gmail.com>
2025-07-22 16:31:09 +0530

    add

--- Environment/ECS/ALB.tf
+++ Environment/ECS/ALB.tf
@@ -24,6 +24,8 @@ resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 443
   protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.acm_certificate_arn
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
