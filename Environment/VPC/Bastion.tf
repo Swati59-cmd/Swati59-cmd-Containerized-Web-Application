712f183ce7ee848ee1ca84d5d02ea5838de1be46
swati <swatihivanse78@gmail.com>
2025-07-24 16:36:14 +0530

    adding

--- Environment/VPC/Bastion.tf
+++ Environment/VPC/Bastion.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion" {
   ami                    = var.ami
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.public[0]
+  subnet_id              = aws_subnet.public[0].id
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
