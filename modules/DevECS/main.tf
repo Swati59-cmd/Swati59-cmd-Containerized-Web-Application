dd2541eccc8126966ee1792f824fbcab82e7f97d
swati <swatihivanse78@gmail.com>
2025-07-31 15:05:40 +0530

    adding

--- modules/DevECS/main.tf
+++ modules/DevECS/main.tf
@@ -1,5 +1,5 @@
 module "ECSdemo" {
-  source        = "../../Environment/ECS"
+  source        = "../../Environment/dev/ECS"
   environment   = "dev"
   ecr_repo_name = "devpython"
   image_tag     = var.image_tag
