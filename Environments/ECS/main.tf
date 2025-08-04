eeed2bd6cfdf8cb02677d2b9cfe51c993f625153
swati <swatihivanse78@gmail.com>
2025-08-04 20:05:45 +0530

    aidn

--- Environments/ECS/main.tf
+++ Environments/ECS/main.tf
@@ -150,7 +150,7 @@ data "aws_ssm_parameter" "ecs_ami" {
 }
 
 module "vpc_main" {
-  source               = "../../../modules/MainVPC"
+  source               = "../../modules/VPC"
   vpc_cidr             = "10.2.0.0/16"
   public_subnet_cidrs  = ["10.2.10.0/24", "10.2.11.0/24"]
   private_subnet_cidrs = ["10.2.20.0/24", "10.2.21.0/24"]
--- modules/ECS/main.tf
+++ modules/ECS/main.tf
@@ -1,5 +1,5 @@
 module "EcsDemo" {
-  source        = "../../Environment/main/ECS"
+  source        = "../../Environments/ECS"
   environment   = var.environment
   ecr_repo_name = "mainpython"
   image_tag     = var.image_tag
