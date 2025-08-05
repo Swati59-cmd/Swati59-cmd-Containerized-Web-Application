76ba0ff1120538a953cc6cf8e5f5d1830c733ca6
swati <swatihivanse78@gmail.com>
2025-08-05 11:09:16 +0530

    :chnagesin

--- Environment/ECS/main.tf
+++ Environment/ECS/main.tf
@@ -157,7 +157,7 @@ data "aws_ssm_parameter" "ecs_ami" {
 
 
 module "vpc_stage" {
-  source               = "../../modules/VPC"
+  source               = "../VPC"
   vpc_cidr             = "10.1.0.0/16"
   public_subnet_cidrs  = ["10.1.10.0/24", "10.1.11.0/24"]
   private_subnet_cidrs = ["10.1.20.0/24", "10.1.21.0/24"]
