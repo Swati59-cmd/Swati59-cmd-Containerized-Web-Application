29d7e238a8c38acf5bfab713ed41ea7b9a37e149
swati <swatihivanse78@gmail.com>
2025-08-11 18:31:18 +0530

    pushing all infrato dev

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -20,7 +20,7 @@ module "sequrity" {
 }
 
 module "vpc" {
-  source               = "../../modules/vpc"
+  source               = "../../modules/VPC"
   vpc_cidr             = var.vpc_cidr
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
