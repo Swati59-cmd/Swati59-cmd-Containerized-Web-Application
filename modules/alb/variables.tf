4d8db442cb8e99ec5561dab18d28b3308ae7a1f1
swati <swatihivanse78@gmail.com>
2025-08-12 16:15:48 +0530

    adding

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -9,7 +9,7 @@ variable "ami_id" {
   type        = string
 
 }
-variable "alb_listener" {}
+
 
 
 variable "acm_certificate_arn" {
@@ -17,5 +17,18 @@ variable "acm_certificate_arn" {
   type        = string
 
 }
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
 
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
 
