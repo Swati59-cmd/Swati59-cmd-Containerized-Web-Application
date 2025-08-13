beecd80a3c889e19b8b0d000d6751aa67cf83118
swati <swatihivanse78@gmail.com>
2025-08-13 15:17:32 +0530

    adding

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,6 +1,6 @@
 # Create VPC
 module "vpc" {
-  source               = "../../modules/vpc"
+  source               = "../../modules/VPC"
   environment          = var.environment
   vpc_cidr             = var.vpc_cidr
   public_subnet_cidrs  = var.public_subnet_cidrs
@@ -26,14 +26,14 @@ module "albdemo" {
 }
 
 module "ecs" {
-  source             = "../../modules/ecs"
+  source             = "../../modules/ECS"
   environment        = var.environment
   ecr_repo_name      = var.ecr_repo_name
   ami_id             = var.ami_id
   instance_type      = var.instance_type
   key_name           = var.key_name
-  ecs_sg_ids         = [module.security.ecs_sg_id]
-  private_subnet_ids = module.vpc.private_subnet_ids
-  target_group_arn   = module.alb.target_group_arn
-  alb_listener_arn   = module.alb.alb_arn
+  ecs_sg_ids         = modules.security.alb_sg_id
+  private_subnet_ids = modules.VPC.public_subnet_ids
+  target_group_arn   = modules.alb.target_group_arn
+  alb_listener_arn   = modules.alb.alb_arn
 }
