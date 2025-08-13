39fb5d20548ea866db2a402bc5357ddddf5610fe
swati <swatihivanse78@gmail.com>
2025-08-13 19:35:24 +0530

    add

--- modules/sequrity/outputs.tf
+++ modules/sequrity/outputs.tf
@@ -1,6 +1,7 @@
 output "ecs_instance_sg_id" {
   value = aws_security_group.ecs_instance_sg.id
 }
-output "ecs-alb-sg" {
-  value = aws_security_group.alb_sg.id
+
+output "ecs_alb_sg_id" {
+  value = aws_security_group.ecs_alb_sg.id
 }
