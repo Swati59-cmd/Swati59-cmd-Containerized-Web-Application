82d151a557bb91637cd4b3b04791dc5d878cb450
swati <swatihivanse78@gmail.com>
2025-08-14 11:06:03 +0530

    adding

--- modules/ECS/main.tf
+++ modules/ECS/main.tf
@@ -90,8 +90,10 @@ resource "aws_ecs_capacity_provider" "ecs_cp" {
   name = "${var.environment}-ecs-cp"
 
   auto_scaling_group_provider {
-    auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
-    managed_termination_protection = "ENABLED"
+    auto_scaling_group_arn = aws_autoscaling_group.ecs.arn
+
+    managed_termination_protection = "DISABLED"
+
 
     managed_scaling {
       status                    = "ENABLED"
