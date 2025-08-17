50b73f36c7e7ed8574efb95ba623d0d09fc75cab
swati <swatihivanse78@gmail.com>
2025-08-17 18:02:18 +0530

    adding

--- modules/ECS/main.tf
+++ modules/ECS/main.tf
@@ -149,9 +149,4 @@ data "aws_ssm_parameter" "ecs_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
 
-module "vpc_main" {
-  source               = "../VPC"
-  vpc_cidr             = "10.2.0.0/16"
-  public_subnet_cidrs  = ["10.2.10.0/24", "10.2.11.0/24"]
-  private_subnet_cidrs = ["10.2.20.0/24", "10.2.21.0/24"]
-}
+
