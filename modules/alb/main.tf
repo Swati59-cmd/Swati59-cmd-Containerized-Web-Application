b402b5da469e2ef1645df4f54ce895eee81628a7
swati <swatihivanse78@gmail.com>
2025-08-18 14:13:18 +0530

    add

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -50,8 +50,8 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.acm_certificate_arn
+  //ssl_policy        = "ELBSecurityPolicy-2016-08"
+  //certificate_arn   = var.acm_certificate_arn
 
   default_action {
     type             = "forward"
