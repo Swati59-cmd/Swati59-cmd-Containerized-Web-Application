3176503e1f3c409592a4c44b5d932aa56976d11f
swati <swatihivanse78@gmail.com>
2025-08-18 10:48:15 +0530

    add

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -47,7 +47,7 @@ resource "aws_lb_target_group" "ecs_tg" {
 ############################################
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
-  port              = 80
+  port              = 443
   protocol          = "HTTP"
 
   default_action {
