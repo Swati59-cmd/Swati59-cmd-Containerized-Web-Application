cacf97cc742e1eeee0349510eadcd3228e5ba078
swati <swatihivanse78@gmail.com>
2025-08-19 12:12:40 +0530

    adding

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -16,7 +16,7 @@ resource "aws_lb" "this" {
 
 resource "aws_lb_target_group" "ecs_tg" {
   name        = "${var.environment}-ecs-tg"
-  port        = 80
+  port        = 5000
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc_id
@@ -35,7 +35,21 @@ resource "aws_lb_target_group" "ecs_tg" {
     Environment = var.environment
   }
 }
+resource "aws_lb_listener" "http_redirect" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
 
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
