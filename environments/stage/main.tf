33fb9e3f60333d22d27966687b0d461ad25a3a74
swati <swatihivanse78@gmail.com>
2025-08-21 19:47:49 +0530

    addingallcode to main

--- environments/stage/main.tf
+++ environments/stage/main.tf
@@ -40,14 +40,14 @@ module "ecs" {
   ami_id        = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
-  //ecs_sg_ids         = [module.securitydemo1.ecs_sg_ids]
-  //private_subnet_ids = module.vpc.private_subnet_ids
-  //target_group_arn   = module.albdemo.target_group_arn
-  //alb_listener_arn   = module.albdemo.alb_listener_arn
+  ecs_sg_ids         = [module.securitydemo1.ecs_sg_ids]
+  private_subnet_ids = module.vpc.private_subnet_ids
+  target_group_arn   = module.albdemo.target_group_arn
+  alb_listener_arn   = module.albdemo.alb_listener_arn
   aws_region     = var.aws_region
   aws_account_id = var.aws_account_id
   depends_on     = [module.albdemo]
-  //public_subnet_ids  = module.vpc.public_subnet_ids
+  public_subnet_ids  = module.vpc.public_subnet_ids
 
 
 
